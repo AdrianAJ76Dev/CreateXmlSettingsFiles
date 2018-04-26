7cdb139020f3d1f8acca96f0e7793287e00a7e2e
AdrianAJ76Dev <AdrianAJ76Dev@gmail.com>
2018-04-26 01:36:25 -0400

    Creating Xml file for settings.

--- CreateXmlSettingsFiles/Program.cs
+++ CreateXmlSettingsFiles/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,58 @@ namespace CreateXmlSettingsFiles
     {
         static void Main(string[] args)
         {
+            CreateSSLSettings();
+        }
+
+        static void CreateSSLSettings()
+        {
+            XElement SSLSettings =
+                new XElement("Settings_SSL",
+                    new XElement("Dev_Machine",
+                        new XAttribute("Machine_Name", "Motown-Mobile-v"),
+                        new XElement("TemplatePath", @"C:\Users\Adria\Documents\Dev Projects\SSL\Documents\"),
+                        new XElement("DocumentPath", @"C:\Users\Adria\Documents\Dev Projects\SSL\Documents\"),
+                        new XElement("Path", @"C:\Users\Adria\Documents\Dev Projects\SSL\Documents\"),
+                        new XElement("XmlPath", @"C:\Users\Adria\Documents\Dev Projects\SSL\Documents\")
+                        ),
+
+                    new XElement("Dev_Machine",
+                        new XAttribute("Machine_Name", "PZ00GS3"),
+                        new XElement("TemplatePath", @"C:\Users\ajones\Documents\Automation\Code\Word\SSL Work\"),
+                        new XElement("DocumentPath", @"C:\Users\ajones\Documents\Automation\Code\Word\SSL Work\"),
+                        new XElement("Path", @"C:\Users\ajones\Documents\Automation\Code\Word\SSL Work\"),
+                        new XElement("XmlPath",@"C:\Users\ajones\Documents\Automation\Code\Word\SSL Work\")
+                        ),
+
+                    new XElement("TemplateName", "Sole Source Letter v6.dotx"),
+                    new XElement("DocumentName", "CB Sole Source Letter.docx"),
+                    new XElement("Xmlfilename", "SSL.xml")
+                );
+
+            Console.WriteLine("This Machine is {0}", Environment.MachineName);
+            Console.WriteLine();
+
+            string settingsfilepath = string.Empty;
+            foreach (XElement el in SSLSettings.Elements("Dev_Machine"))
+            {
+                Console.WriteLine("Current Machine Name is {0}", el.Attribute("Machine_Name").Value);
+                if (el.Attribute("Machine_Name").Value.ToLower() == Environment.MachineName.ToLower())
+                {
+                    settingsfilepath = el.Element("XmlPath").Value + SSLSettings.Element("Xmlfilename").Value;
+                    Console.WriteLine("This Element Value is {0}",el.Value);
+                }
+            }
+
+            // Using Linq
+            Console.WriteLine("{0}", Environment.MachineName.ToString());
+            var dev_machine = (from el in SSLSettings.Elements("Dev_Machine")
+                                                where el.Attribute("Machine_Name").Value == Environment.MachineName.ToString()
+                                                select el).Single();
+
+            SSLSettings.Save(dev_machine.Element("DocumentPath").Value + dev_machine.Element("DocumentName"));
+
+            Console.WriteLine("Saved Xml file");
+            Console.Read();
         }
     }
 }
